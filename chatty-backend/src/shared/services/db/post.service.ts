47dd243fbc7321591af3364d0c24824dfe994f57
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-07-10 22:51:17 +0600

    implement getting post from cache and db

--- chatty-backend/src/shared/services/db/post.service.ts
+++ chatty-backend/src/shared/services/db/post.service.ts
@@ -12,6 +12,7 @@ import { UserModel } from '@user/models/user.schema'
 import { Query, UpdateQuery } from 'mongoose'
 
 class PostService {
+  // Create post and save to db
   public async addPostToDB(
     userId: string,
     createdPost: IPostDocument
@@ -24,55 +25,57 @@ class PostService {
     await Promise.all([post, user])
   }
 
-//   public async getPosts(
-//     query: IGetPostsQuery,
-//     skip = 0,
-//     limit = 0,
-//     sort: Record<string, 1 | -1>
-//   ): Promise<IPostDocument[]> {
-//     let postQuery = {}
-//     if (query?.imgId && query?.gifUrl) {
-//       postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] }
-//     } else if (query?.videoId) {
-//       postQuery = { $or: [{ videoId: { $ne: '' } }] }
-//     } else {
-//       postQuery = query
-//     }
-//     const posts: IPostDocument[] = await PostModel.aggregate([
-//       { $match: postQuery },
-//       { $sort: sort },
-//       { $skip: skip },
-//       { $limit: limit },
-//     ])
-//     return posts
-//   }
+  // Get post from db
+  public async getPosts(
+    query: IGetPostsQuery,
+    skip = 0,
+    limit = 0,
+    sort: Record<string, 1 | -1>
+  ): Promise<IPostDocument[]> {
+    let postQuery = {}
+    if (query?.imgId && query?.gifUrl) {
+      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] }
+    } else if (query?.videoId) {
+      postQuery = { $or: [{ videoId: { $ne: '' } }] }
+    } else {
+      postQuery = query
+    }
+    const posts: IPostDocument[] = await PostModel.aggregate([
+      { $match: postQuery },
+      { $sort: sort },
+      { $skip: skip },
+      { $limit: limit },
+    ])
+    return posts
+  }
 
-//   public async postsCount(): Promise<number> {
-//     const count: number = await PostModel.find({}).countDocuments()
-//     return count
-//   }
+  // Get post count from db
+  public async postsCount(): Promise<number> {
+    const count: number = await PostModel.find({}).countDocuments()
+    return count
+  }
 
-//   public async deletePost(postId: string, userId: string): Promise<void> {
-//     const deletePost: Query<IQueryComplete & IQueryDeleted, IPostDocument> =
-//       PostModel.deleteOne({ _id: postId })
-//     // delete reactions here
-//     const decrementPostCount: UpdateQuery<IUserDocument> = UserModel.updateOne(
-//       { _id: userId },
-//       { $inc: { postsCount: -1 } }
-//     )
-//     await Promise.all([deletePost, decrementPostCount])
-//   }
+  //   public async deletePost(postId: string, userId: string): Promise<void> {
+  //     const deletePost: Query<IQueryComplete & IQueryDeleted, IPostDocument> =
+  //       PostModel.deleteOne({ _id: postId })
+  //     // delete reactions here
+  //     const decrementPostCount: UpdateQuery<IUserDocument> = UserModel.updateOne(
+  //       { _id: userId },
+  //       { $inc: { postsCount: -1 } }
+  //     )
+  //     await Promise.all([deletePost, decrementPostCount])
+  //   }
 
-//   public async editPost(
-//     postId: string,
-//     updatedPost: IPostDocument
-//   ): Promise<void> {
-//     const updatePost: UpdateQuery<IPostDocument> = PostModel.updateOne(
-//       { _id: postId },
-//       { $set: updatedPost }
-//     )
-//     await Promise.all([updatePost])
-//   }
+  //   public async editPost(
+  //     postId: string,
+  //     updatedPost: IPostDocument
+  //   ): Promise<void> {
+  //     const updatePost: UpdateQuery<IPostDocument> = PostModel.updateOne(
+  //       { _id: postId },
+  //       { $set: updatedPost }
+  //     )
+  //     await Promise.all([updatePost])
+  //   }
 }
 
 export const postService: PostService = new PostService()
