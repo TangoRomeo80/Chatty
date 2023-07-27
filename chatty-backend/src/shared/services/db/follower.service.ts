e72f18674d8f3e6663852b5a23af00730b128448
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-07-27 12:37:32 +0600

    implement method to get follower and followee information from mongodb

--- chatty-backend/src/shared/services/db/follower.service.ts
+++ chatty-backend/src/shared/services/db/follower.service.ts
@@ -84,6 +84,120 @@ class FollowerService {
 
     await Promise.all([unfollow, users])
   }
+
+  // Function to get followee data
+  public async getFolloweeData(
+    userObjectId: ObjectId
+  ): Promise<IFollowerData[]> {
+    const followee: IFollowerData[] = await FollowerModel.aggregate([
+      { $match: { followerId: userObjectId } },
+      {
+        $lookup: {
+          from: 'User',
+          localField: 'followeeId',
+          foreignField: '_id',
+          as: 'followeeId',
+        },
+      },
+      { $unwind: '$followeeId' },
+      {
+        $lookup: {
+          from: 'Auth',
+          localField: 'followeeId.authId',
+          foreignField: '_id',
+          as: 'authId',
+        },
+      },
+      { $unwind: '$authId' },
+      {
+        $addFields: {
+          _id: '$followeeId._id',
+          username: '$authId.username',
+          avatarColor: '$authId.avatarColor',
+          uId: '$authId.uId',
+          postCount: '$followeeId.postsCount',
+          followersCount: '$followeeId.followersCount',
+          followingCount: '$followeeId.followingCount',
+          profilePicture: '$followeeId.profilePicture',
+          userProfile: '$followeeId',
+        },
+      },
+      {
+        $project: {
+          authId: 0,
+          followerId: 0,
+          followeeId: 0,
+          createdAt: 0,
+          __v: 0,
+        },
+      },
+    ])
+    return followee
+  }
+
+  // Function to get follower data
+  public async getFollowerData(
+    userObjectId: ObjectId
+  ): Promise<IFollowerData[]> {
+    const follower: IFollowerData[] = await FollowerModel.aggregate([
+      { $match: { followeeId: userObjectId } },
+      {
+        $lookup: {
+          from: 'User',
+          localField: 'followerId',
+          foreignField: '_id',
+          as: 'followerId',
+        },
+      },
+      { $unwind: '$followerId' },
+      {
+        $lookup: {
+          from: 'Auth',
+          localField: 'followerId.authId',
+          foreignField: '_id',
+          as: 'authId',
+        },
+      },
+      { $unwind: '$authId' },
+      {
+        $addFields: {
+          _id: '$followerId._id',
+          username: '$authId.username',
+          avatarColor: '$authId.avatarColor',
+          uId: '$authId.uId',
+          postCount: '$followerId.postsCount',
+          followersCount: '$followerId.followersCount',
+          followingCount: '$followerId.followingCount',
+          profilePicture: '$followerId.profilePicture',
+          userProfile: '$followerId',
+        },
+      },
+      {
+        $project: {
+          authId: 0,
+          followerId: 0,
+          followeeId: 0,
+          createdAt: 0,
+          __v: 0,
+        },
+      },
+    ])
+    return follower
+  }
+
+  // Function to get followee ids
+  public async getFolloweesIds(userId: string): Promise<string[]> {
+    const followee = await FollowerModel.aggregate([
+      { $match: { followerId: new mongoose.Types.ObjectId(userId) } },
+      {
+        $project: {
+          followeeId: 1,
+          _id: 0,
+        },
+      },
+    ])
+    return map(followee, (result) => result.followeeId.toString())
+  }
 }
 
 export const followerService: FollowerService = new FollowerService()
