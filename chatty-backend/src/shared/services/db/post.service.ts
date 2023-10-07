1309bd4942f07128ac8bbbb2902de47e6768eb89
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-10-07 17:40:12 +0600

    Update post.service.ts

--- chatty-backend/src/shared/services/db/post.service.ts
+++ chatty-backend/src/shared/services/db/post.service.ts
@@ -12,7 +12,6 @@ import { UserModel } from '@user/models/user.schema'
 import { Query, UpdateQuery } from 'mongoose'
 
 class PostService {
-  // Create post and save to db
   public async addPostToDB(
     userId: string,
     createdPost: IPostDocument
@@ -25,7 +24,6 @@ class PostService {
     await Promise.all([post, user])
   }
 
-  // Get posts from db
   public async getPosts(
     query: IGetPostsQuery,
     skip = 0,
@@ -49,13 +47,11 @@ class PostService {
     return posts
   }
 
-  // Get post count from db
   public async postsCount(): Promise<number> {
     const count: number = await PostModel.find({}).countDocuments()
     return count
   }
 
-  // Delete post from db
   public async deletePost(postId: string, userId: string): Promise<void> {
     const deletePost: Query<IQueryComplete & IQueryDeleted, IPostDocument> =
       PostModel.deleteOne({ _id: postId })
@@ -67,7 +63,6 @@ class PostService {
     await Promise.all([deletePost, decrementPostCount])
   }
 
-  // Edit post in db
   public async editPost(
     postId: string,
     updatedPost: IPostDocument
